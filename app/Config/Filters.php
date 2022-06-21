1fb3737d0a95254efa5308d0fd579e78768395df
emarosida20 <101863671+emarosida20@users.noreply.github.com>
2022-06-21 09:06:11 +0700

    Add files via upload

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -6,8 +6,6 @@ use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
-use CodeIgniter\Filters\InvalidChars;
-use CodeIgniter\Filters\SecureHeaders;
 
 class Filters extends BaseConfig
 {
@@ -21,8 +19,7 @@ class Filters extends BaseConfig
         'csrf'          => CSRF::class,
         'toolbar'       => DebugToolbar::class,
         'honeypot'      => Honeypot::class,
-        'invalidchars'  => InvalidChars::class,
-        'secureheaders' => SecureHeaders::class,
+        'auth'          => App\Filters\Auth::class,
     ];
 
     /**
---
+++ app/Views/template/admin_footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>&copy; Ema Rosida - 312010062 - TI.20.D1 - 2022 - Universitas Pelita Bangsa</p>
+    </footer>
+    </div>
+</body>
+</html>
