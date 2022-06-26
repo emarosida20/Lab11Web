4530cb26f050b2a2f2be664f13927334b2f5543f
emarosida20 <101863671+emarosida20@users.noreply.github.com>
2022-06-26 19:14:33 +0700

    Add files via upload

--- app/Views/artikel/admin_index.php
+++ app/Views/artikel/admin_index.php
@@ -1,5 +1,10 @@
 <?= $this->include('template/admin_header'); ?>
 
+<form method="get" class="form-search">
+    <input type="text" name="q" value="<?= $q; ?>" placeholder="Cari data">
+    <input type="submit" value="Cari" class="btn btn-primary">
+</form>
+
 <table class="table">
     <thead>
         <tr>
@@ -15,7 +20,7 @@
         <td><?= $row['id']; ?></td>
         <td>
             <b><?= $row['judul']; ?></b>
-            <p><small><?= substr($row['isi'], 0, 50); ?></small></p>
+            <p><small><?= substr ($row['isi'], 0, 50); ?></small></p>
         </td>
         <td><?= $row['status']; ?></td>
         <td>
@@ -39,5 +44,7 @@
         </tr>
     </tfoot>
 </table>
+
+<?= $pager->only(['q'])->links(); ?>
 
 <?= $this->include('template/admin_footer'); ?>
